5724026ff1a0062ddfd3197289ac009c18a850bb
stef-spears <spears.sj87@gmail.com>
2019-10-29 23:44:43 -0500

    continue to ask for input

--- lib/CTA/cli.rb
+++ lib/CTA/cli.rb
@@ -20,27 +20,23 @@ A list of stations is below. To exit, type "exit".'
     end
 
     def stations_list
-        
-         CTA::Stations.all.each do |station|
+        CTA::Stations.all.each do |station|
             @@list << station.station_name
         end
         puts @@list.sort.uniq
     end
 
     def get_input
-        
         puts "Enter a station name:"
-        
         @user_input = gets.strip.capitalize
-      
     end
 
     def validate_input
         # if @user_input == "Exit"
-        #     exit
+        #     puts "Thank you for riding the CTA!"
+        #     exit!
         # end
-
-        while !@@list.include?(@user_input)
+        while @user_input != "Exit" && !@@list.include?(@user_input)
                 puts "That is not a valid station name. Please try again!"
                 @user_input == nil
                 get_input
@@ -49,30 +45,23 @@ A list of stations is below. To exit, type "exit".'
     end 
 
     def answer
-        # look inside of CTA::Stations.all
-        # find the hash object that has a station_name that equals @user_input
-        # if that hash object's ada property is true
-        # puts "is accessible"
-        # else put "isn't accessible"
+    
         CTA::Stations.all.each do |station|
             if station.station_name == @user_input && station.ada == false
                 puts "This station is not accessible."
+                get_input
             elsif station.station_name == @user_input && station.ada == true
                 puts "This station is accessible."
+                get_input
             end
-        end 
-             #binding.pry
-    #          if station == @user_input && station.ada == false
-    #             
-    # #                 else puts "This station is accessible.".
-    #     end
-             
+           
+        end  
+        # stations_list
+        # get_input      
     end 
 
- 
-
-
     # def quit
+    #     puts "Thank you for riding the CTA!"
     # end
 
 end
