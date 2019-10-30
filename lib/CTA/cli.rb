9be18c43004f47b7eba43dd9fccdd244d224adfc
stef-spears <spears.sj87@gmail.com>
2019-10-29 23:08:36 -0500

    defined answer, removed duplicate results from API call

--- lib/CTA/cli.rb
+++ lib/CTA/cli.rb
@@ -36,24 +36,41 @@ A list of stations is below. To exit, type "exit".'
     end
 
     def validate_input
+        # if @user_input == "Exit"
+        #     exit
+        # end
+
         while !@@list.include?(@user_input)
                 puts "That is not a valid station name. Please try again!"
                 @user_input == nil
                 get_input
         end
         answer
-        
     end 
 
     def answer
-    #     CTA::Stations.all.each do |station|
-    #         binding.pry
-    #         if station.ada == false
-    #             puts "This station is not accessible."
-    #                 else puts "This station is accessible.".
-    #         end
-    #     end    
+        # look inside of CTA::Stations.all
+        # find the hash object that has a station_name that equals @user_input
+        # if that hash object's ada property is true
+        # puts "is accessible"
+        # else put "isn't accessible"
+        CTA::Stations.all.each do |station|
+            if station.station_name == @user_input && station.ada == false
+                puts "This station is not accessible."
+            elsif station.station_name == @user_input && station.ada == true
+                puts "This station is accessible."
+            end
+        end 
+             #binding.pry
+    #          if station == @user_input && station.ada == false
+    #             
+    # #                 else puts "This station is accessible.".
+    #     end
+             
     end 
+
+ 
+
 
     # def quit
     # end
--- lib/CTA/stations.rb
+++ lib/CTA/stations.rb
@@ -1,34 +1,16 @@
 class CTA::Stations
 
-attr_accessor :station_name, :ada
-@@all = []
+    attr_accessor :station_name, :ada
+    @@all = []
 
-def initialize(station_name, ada)
-    @station_name = station_name
-    @ada = ada
-    @@all << self
-end
+    def initialize(station_name, ada)
+        @station_name = station_name
+        @ada = ada
+        @@all << self
+    end
 
-def self.all
-    @@all
-end 
-
-
-#intro
-#list station names
-# asks for user input, says type exit to exit
-# gets input from user, capitalizes
-
-# if user types exit, exit the program
-
-# if list of stations !include? [user response]
-    #p "invalid response"
-    #back to list station names
-
-# if list of stations include? [user response]
-    # response.ada = true return "is accessible"
-        # else return "not accessible"
-
-# return to list station names
+    def self.all
+        @@all.uniq
+    end 
 
 end 
