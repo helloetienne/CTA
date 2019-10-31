54db26b8d98f82069e86b3205b628fa1284104ca
stef-spears <spears.sj87@gmail.com>
2019-10-30 21:33:49 -0500

    changed api filter to broaden returned data

--- lib/CTA/api.rb
+++ lib/CTA/api.rb
@@ -2,7 +2,7 @@ class CTA::API
 
     def fetch
         # token = "6QpIAffmCHjY6PG5cgrK6VKQ3" - this lets me make more calls if needed. not using for now
-        url = "https://data.cityofchicago.org/resource/8pix-ypme.json?blue=true&direction_id=N"
+        url = "https://data.cityofchicago.org/resource/8pix-ypme.json"
        
         response = HTTParty.get(url)
         response.each do |station|
